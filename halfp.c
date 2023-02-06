200dc530030fe5043b6492f775d53e6dc3132180
sushi1694 <124113986+sushi1694@users.noreply.github.com>
2023-02-06 09:34:45 +0530

    Create halfp.c

---
+++ halfp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+    printf("Enter the number of rows: \n");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++)
+    {
+
+        for (j = 1; j <= rows; j++)
+        {
+            if ((i+j)<=rows)
+            {
+                 
+                printf(" ");
+            }
+            else
+            {
+                
+                printf("*");
+            }
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
